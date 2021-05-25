5fe402370aa4d95e4e30953b010c8b3f60d836db
Evan-Leon <evan.leon.j@gmail.com>
2021-05-25 16:07:51 -0400

    Completed suggestion schema

--- models/Suggestion.js
+++ models/Suggestion.js
@@ -1,20 +1,31 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const SuggestionSchema = new Schema({
-//     title: {
-//         type: String,
-//         required: true
-//     },
-//     description: {
-//         type: String
-//     },
-//     startDate: {
-//         type: [String], // default: []
-//         required: true
-//     }
-// }, {
-//     timestamps: true
-// })
+const SuggestionSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    budget: {
+        type: [String],
+    },
+    dates: {
+        type: [Date], // default: []
+        
+    },
+    plan: [{
+        type: Schema.Types.ObjectID,
+        ref: Plan
+    }],
+    user: [{
+        type: Schema.Types.ObjectID,
+        ref: User
+    }]
+}, {
+    timestamps: true
+})
 
-// module.exports = Plan = mongoose.model('Suggestion', SuggestionSchema);
+module.exports = Plan = mongoose.model('Suggestion', SuggestionSchema);

3fdade022f186dca38c60db2816c16f607faf3e7
michelleroos <hello.michelleroos@gmail.com>
2021-05-25 10:45:37 -0400

    plan routes for patch x2 and delete

--- routes/api/plans.js
+++ routes/api/plans.js
@@ -25,8 +25,6 @@ router.post('/',
     }
 );
 
-
-
 router.get('/user/:user_id', (req, res) => {
     Plan.find({members: req.params.user_id})
         .then(plans => res.json(plans))
@@ -44,10 +42,62 @@ router.get('/:id', (req, res) => {
         );
 });
 
-// router.delete('/:id', (req, res) => {
-//     Plan.findById(req.params.id)
-      
-// })
+router.delete('/:id', (req, res) => {
+    const planId = req.params.id;
+    Plan.deleteOne({ _id: planId })
+        .then(() => res.status(200).json({ plandeleted: 'plan successfully deleted' }))
+        .catch(err =>
+            res.status(404).json({ noplanfound: 'No plan found with that id, please try again' })
+        );
+})
+
+router.patch('/:id/addmember',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+
+        const currUser = req.user;
+        const planId = { _id: req.params.id };
+        const update = { members: currUser }
+
+        Plan.findOneAndUpdate(
+            planId, { $push: update }, { new: true })
+                .then(plan => res.json(plan))
+                .catch(err =>
+                    res.status(404).json({ noplanfound: 'No plan found with that id, please try again' })
+                );
+    }
+);
+
+router.patch('/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+
+        const title = req.body.title;
+        const description = req.body.description;
+        const planId = { _id: req.params.id };
+        let update = { 
+            title: title,
+            description: description
+        }
+        
+        if (!title) {
+            update = {
+                description: description
+            }
+        } else if (!description) {
+            update = {
+                title: title
+            }
+        }
+
+        Plan.findOneAndUpdate(
+            planId, update, { new: true })
+                .then(plan => res.json(plan))
+                .catch(err =>
+                    res.status(404).json({ noplanfound: 'No plan found with that id, please try again' })
+                );
+    }
+);
 
 
 

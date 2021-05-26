6659a95a882137be1fdaec292b1b736d628eb235
jessicaUP <uphoff.jessica@gmail.com>
2021-05-26 08:51:00 -0400

    Heroku change

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,8 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
-    google: process.env.GOOGLE
+    google_client_id: process.env.GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    google_redirect_uri: process.env.GOOGLE_REDIRECT_URI,
+    google_refresh_token: process.env.GOOGLE_REFRESH_TOKEN
 }

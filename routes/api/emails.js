5176adb189dc0ef36f29eb5c082810ff2a82d8c9
michelleroos <hello.michelleroos@gmail.com>
2021-05-25 21:20:55 -0400

    removed sensitive info

--- routes/api/emails.js
+++ routes/api/emails.js
@@ -3,11 +3,6 @@ const router = express.Router();
 var nodemailer = require('nodemailer');
 var { google } = require('googleapis');
 
-const CLIENT_ID = "957768252496-6pa4oe3v07ttsk83p3c3fd47jihscd32.apps.googleusercontent.com";
-const CLIENT_SECRET = "LDz6xC-kxvZR95PZDmbTQ9XI";
-const REDIRECT_URI = "https://developers.google.com/oauthplayground";
-const REFRESH_TOKEN = "1//048N_eApYLQQVCgYIARAAGAQSNwF-L9IrnUQv__RT9sZQ5w4YyMA3fPDI3OjJgNP1-4AdnFumjmxh6hZ1DQ4-3NJdU7bkLCl8D8c";
-
 const oAuth2Client = new google.auth.OAuth2(
     CLIENT_ID,
     CLIENT_SECRET,

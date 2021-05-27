a2176a23ea26c1de1658e6da7f322e216c230a0f
syldysnya <syldysnya@gmail.com>
2021-05-27 01:06:26 -0400

    Change plan suggestion creation an dpoping up right after it

--- frontend/src/components/plans/plan/plan_item_container.js
+++ frontend/src/components/plans/plan/plan_item_container.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import { openModal } from "../../../actions/modal_actions";
 import { fetchPlan, joinParty, sendInvite } from "../../../actions/plan_actions";
+import { fetchPlanSuggs } from "../../../actions/sugg_actions";
 import PlanItem from "./plan_item";
 
 
@@ -8,7 +9,8 @@ const mapSTP = (state) => {
     
     return ({
         plan: state.plans.new,
-        currentUser: state.session.user
+        currentUser: state.session.user,
+        planSuggs: state.suggs.planSuggs
     })
 };
 
@@ -18,7 +20,8 @@ const mapDTP = dispatch => {
         fetchPlan: id => dispatch(fetchPlan(id)),
         sendInvite: (email, id) => dispatch(sendInvite(email, id)),
         joinParty: (planId, userId) => dispatch(joinParty(planId, userId)),
-        openModal: modal => dispatch(openModal(modal))
+        openModal: modal => dispatch(openModal(modal)),
+        fetchPlanSuggs: planId => dispatch(fetchPlanSuggs(planId))
     })
 };
 

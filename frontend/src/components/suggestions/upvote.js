86f5fcc57ab0737c9f96ec15afdd156b271e137b
syldysnya <syldysnya@gmail.com>
2021-05-27 16:58:24 -0400

    Fix stuff

--- frontend/src/components/suggestions/upvote.js
+++ frontend/src/components/suggestions/upvote.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 class Upvote extends React.Component {
 
-    constructor(props) {
-        super(props)
-        let up = props.upvotes.length;
-        let down = props.downvotes.length;
+    // constructor(props) {
+    //     super(props)
+    //     let up = props.upvotes.length;
+    //     let down = props.downvotes.length;
         
-        this.state = {
-            numUpvotes: up,
-            numDownVotes: down,
-            numDiff: (up - down)
-        }
-    }
+    //     this.state = {
+    //         numUpvotes: up,
+    //         numDownVotes: down,
+    //         numDiff: (up - down)
+    //     }
+    // }
 
     render() {
         debugger
         return (
             <div>
                 <button>
-                    {this.props.sugg}
+                    {/* {this.props.sugg} */}
                 </button>
             </div>
         )

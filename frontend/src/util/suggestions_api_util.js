0c27a52bd7a32007bbd66483c8ed5ebc044b4ff3
syldysnya <syldysnya@gmail.com>
2021-05-27 10:57:16 -0400

    Add axios reqs

--- frontend/src/util/suggestions_api_util.js
+++ frontend/src/util/suggestions_api_util.js
@@ -8,6 +8,10 @@ export const showPlanSugg = (planId) => {
     return axios.get(`/api/suggestions/plan/${planId}`)
 };
 
+export const deletePlanSugg = (suggId) => {
+    return axios.get(`/api/suggestions/${suggId}`)
+};
+
 export const createComment = (comment, suggId) => {
     return axios.post(`/api/comments/suggestion/${suggId}/create`, comment)
 };
@@ -18,4 +22,20 @@ export const editComment = comment => {
 
 export const deleteComment = id => {
     return axios.delete(`/api/comments/${id}`)
+};
+
+export const upvote = (suggId) => {
+    return axios.patch(`/api/suggestions/${suggId}/upvote`)
+};
+
+export const upvoteRemove = (suggId) => {
+    return axios.patch(`/api/suggestions/${suggId}/removeupvote`)
+};
+
+export const downvote = (suggId) => {
+    return axios.patch(`/api/suggestions/${suggId}/downvote`)
+};
+
+export const downvoteRemove = (suggId) => {
+    return axios.patch(`/api/suggestions/${suggId}/removedownvote`)
 };
